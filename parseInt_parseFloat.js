2afb70de3f6d05f1bf0ceb8005aae594cb3686f7
muhamadrivaldi1 <muhamadrivaldi577@gmail.com>
2025-04-18 23:42:34 +0700

    belajar dasar dasar js

---
+++ parseInt_parseFloat.js
@@ -0,0 +1,17 @@
+const cm = '20cm';
+const px = '64px';
+
+const intFromCM = parseInt(cm);
+const intFromPX = parseInt(px);
+
+console.log(intFromCM); // output: 20
+console.log(intFromPX); // output: 64
+
+const Cm = '20.55cm';
+const Px = '64.23px';
+
+const floatFromCM = parseFloat(Cm);
+const floatFromPX = parseFloat(Px);
+
+console.log(floatFromCM); // output: 20.55
+console.log(floatFromPX); // output: 64.23
---
+++ ubah_toNumber.js
@@ -0,0 +1,11 @@
+const strNumber = '123';
+const strFloat = '3.14';
+const boolean = true;
+
+const numFromString = Number(strNumber);
+const floatFromString = Number(strFloat);
+const numFromBoolean = Number(boolean);
+
+console.log(numFromString); // output: 123
+console.log(floatFromString); // output: 3.14
+console.log(numFromBoolean); // output: 1
