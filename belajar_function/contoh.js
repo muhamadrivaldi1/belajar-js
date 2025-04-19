7d9760af1d581ac49be304ea8eb808c6c819c196
muhamadrivaldi1 <muhamadrivaldi577@gmail.com>
2025-04-19 21:50:38 +0700

    belajar function js

---
+++ belajar_function/contoh.js
@@ -0,0 +1,16 @@
+// 1. Membuat fungsi 'hello' dengan 2 argumen, 'name' dan 'origin' (dengan nilai default "Bandung")
+function hello(name, origin = "Bandung") {
+    // 2. Mengembalikan string sesuai format
+    return `Halo! Nama saya ${name}. Saya tinggal di ${origin}.`;
+  }
+  
+  // 4. Variabel 'rahmat' memanggil fungsi hello dengan nama dan origin
+  const rival = hello('Rival', 'Tangerang');
+  
+  // 5. Variabel 'indra' memanggil fungsi hello hanya dengan nama (origin akan otomatis "Bandung")
+  const indra = hello('Indra');
+  
+  // Menampilkan hasilnya
+  console.log(rival); // Output: Halo! Nama saya Rahmat. Saya tinggal di Semarang.
+  console.log(indra);  // Output: Halo! Nama saya Indra. Saya tinggal di Bandung.
+  
